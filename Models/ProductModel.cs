7c6b8649498dd9877f6ffaf8a3d0c45819a1454a
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-05 02:24:27 +0000

    Adds Attributes to display descriptions for each of the properties, via System.ComponentModel. Also utilised DataAnnotations to display the currency for Price property.

--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,13 +1,21 @@
-﻿namespace DatabaseAppMockarooData.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseAppMockarooData.Models
 {
     public class ProductModel
     {
+        [DisplayName("Id number")]
         public int Id { get; set; }
 
+        [DisplayName("Product Name")]
         public string Name { get; set; }
 
+        [DisplayName("Cost to customer")]
+        [DataType(DataType.Currency)] // Currency is a data type that includes £/$ signs.
         public decimal Price { get; set; }
 
+        [DisplayName("What you get...")]
         public string Description { get; set; }
     }
 }
