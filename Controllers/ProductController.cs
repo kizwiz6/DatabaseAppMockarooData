d8ce6bcf2e9b569184b48550a02b45675dd54c60
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-05 02:32:46 +0000

    Creating and generating fake data via the Bogus NuGet package. Bogus allows you to load databases, UI and apps with fake data for your testing needs.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseAppMockarooData.Models;
+﻿using Bogus;
+using DatabaseAppMockarooData.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -14,6 +15,18 @@ namespace DatabaseAppMockarooData.Controllers
             productsList.Add(new ProductModel { Id = 2, Name = "Web Cam", Price = 45.50m, Description = "Enables you attend more Zoom meetings" });
             productsList.Add(new ProductModel { Id = 3, Name = "4 TB USB hard drive", Price = 130.00m, Description = "Backup all of your work. You won't regret it." });
             productsList.Add(new ProductModel { Id = 4, Name = "Wireless Mouse", Price = 15.99m, Description = "Notebook mice don't work very well, do they?" });
+
+            // Add some new items.
+            // Take the products list and add an item to it to create a new Faker object.
+            for (int i = 0; i < 100; i++)
+            {
+                productsList.Add(new Faker<ProductModel>()
+                    .RuleFor(p => p.Id, i + 5)
+                    .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                    .RuleFor(p => p.Price, f => f.Random.Decimal(100))
+                    .RuleFor(p => p.Description, f => f.Rant.Review())
+                    );
+            }
 
             return View(productsList);
         }
