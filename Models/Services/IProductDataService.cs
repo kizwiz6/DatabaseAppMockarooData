12156cb65d10b50ce9b78d6a6ad012e34459a714
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-06 22:21:30 +0000

    Creates a new interface that a typical CRUD app has to do.

---
+++ Models/Services/IProductDataService.cs
@@ -0,0 +1,16 @@
+﻿namespace DatabaseAppMockarooData.Models.Services
+{
+    /// <summary>
+    /// Different functions that a typical CRUD app has to do.
+    /// Defines the functions we want to happen.
+    /// </summary>
+    public interface IProductDataService
+    {
+        List<ProductModel> GetAllProducts(); // First item that I want to guarantee that happens
+        List<ProductModel> SearchProducts(string searchTerm); // Returns a list of anything that matches the saerch term.
+        ProductModel GetProductById(int id); // Get product by ID
+        int Insert(ProductModel model); // INSERT
+        int Delete(ProductModel model); // DELETE
+        int Update(ProductModel model); // UPDATE
+    }
+}
