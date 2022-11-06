05206e9c3665ba3bbd68e02e57ed7fdf6a1e37ec
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-06 22:34:41 +0000

    Creates a service to generate a list of all the products.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using DatabaseAppMockarooData.Models;
+using DatabaseAppMockarooData.Models.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -9,26 +10,9 @@ namespace DatabaseAppMockarooData.Controllers
     {
         public IActionResult Index()
         {
-            List<ProductModel> productsList = new List<ProductModel>();
+            HardCodedSampleDataRepository hardCodedSampleDataRepository = new HardCodedSampleDataRepository();
 
-            productsList.Add(new ProductModel { Id = 1, Name = "Mouse Pad", Price = 5.99m, Description = "A square piece of plastic to make mousing easier" });
-            productsList.Add(new ProductModel { Id = 2, Name = "Web Cam", Price = 45.50m, Description = "Enables you attend more Zoom meetings" });
-            productsList.Add(new ProductModel { Id = 3, Name = "4 TB USB hard drive", Price = 130.00m, Description = "Backup all of your work. You won't regret it." });
-            productsList.Add(new ProductModel { Id = 4, Name = "Wireless Mouse", Price = 15.99m, Description = "Notebook mice don't work very well, do they?" });
-
-            // Add some new items.
-            // Take the products list and add an item to it to create a new Faker object.
-            for (int i = 0; i < 100; i++)
-            {
-                productsList.Add(new Faker<ProductModel>()
-                    .RuleFor(p => p.Id, i + 5)
-                    .RuleFor(p => p.Name, f => f.Commerce.ProductName())
-                    .RuleFor(p => p.Price, f => f.Random.Decimal(100))
-                    .RuleFor(p => p.Description, f => f.Rant.Review())
-                    );
-            }
-
-            return View(productsList);
+            return View(hardCodedSampleDataRepository.GetAllProducts());
         }
 
         public IActionResult Message()
--- Models/Services/HardCodedSampleDataRepository.cs
+++ Models/Services/HardCodedSampleDataRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace DatabaseAppMockarooData.Models.Services
+﻿using Bogus;
+
+namespace DatabaseAppMockarooData.Models.Services
 {
     /// <summary>
     /// Hard coded as it it's not going to come from any database.
@@ -9,14 +11,41 @@
     /// </summary>
     public class HardCodedSampleDataRepository : IProductDataService
     {
+        List<ProductModel> productsList = new List<ProductModel>(); // Class member variable.
+
         public int Delete(ProductModel model)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Service that generates a list of all the products.
+        /// References the class member variable for a more globalised view.
+        /// </summary>
+        /// <returns></returns>
         public List<ProductModel> GetAllProducts()
         {
-            throw new NotImplementedException();
+            if (productsList.Count == 0)
+            {
+                productsList.Add(new ProductModel { Id = 1, Name = "Mouse Pad", Price = 5.99m, Description = "A square piece of plastic to make mousing easier" });
+                productsList.Add(new ProductModel { Id = 2, Name = "Web Cam", Price = 45.50m, Description = "Enables you attend more Zoom meetings" });
+                productsList.Add(new ProductModel { Id = 3, Name = "4 TB USB hard drive", Price = 130.00m, Description = "Backup all of your work. You won't regret it." });
+                productsList.Add(new ProductModel { Id = 4, Name = "Wireless Mouse", Price = 15.99m, Description = "Notebook mice don't work very well, do they?" });
+
+                // Add some new items.
+                // Take the products list and add an item to it to create a new Faker object.
+                for (int i = 0; i < 100; i++)
+                {
+                    productsList.Add(new Faker<ProductModel>()
+                        .RuleFor(p => p.Id, i + 5)
+                        .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                        .RuleFor(p => p.Price, f => f.Random.Decimal(100))
+                        .RuleFor(p => p.Description, f => f.Rant.Review())
+                        );
+                }
+            }
+
+            return productsList;
         }
 
         public ProductModel GetProductById(int id)
