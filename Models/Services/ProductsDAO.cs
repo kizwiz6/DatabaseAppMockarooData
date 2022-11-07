9ccb65b3309c73517779be6c8aa26ee6ce376ff9
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-07 15:59:24 +0000

    Creates a database for Mockaroo app data for groceries. Adds a new ProductsDAO class.

---
+++ Models/Services/ProductsDAO.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+
+namespace DatabaseAppMockarooData.Models.Services
+{
+    public class ProductsDAO : IProductDataService
+    {
+        // Connection string to allow us to talk to the database.
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public int Delete(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            // Create a list of products (type ProductModel) for whatever products found.
+            List<ProductModel> foundProducts = new List<ProductModel>();
+
+            string sqlStatement = "SELECT * FROM dbo.Products";
+
+            // using SQL connnection provided with connectionSring. Talks to the database.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        foundProducts.Add(new ProductModel { Id = (int)reader[0], Name = (string)reader[1], Price = (decimal)reader[2], Description = (string)reader[3] });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return foundProducts;
+
+        }
+
+        public ProductModel GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductModel> SearchProducts(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
