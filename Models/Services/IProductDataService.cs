bc2d51c2921dea040a66db4bb635bf35c6d94ad1
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-08 23:45:11 +0000

    Correction for IPRoductDataService to call product not model.

--- Models/Services/IProductDataService.cs
+++ Models/Services/IProductDataService.cs
@@ -9,8 +9,8 @@
         List<ProductModel> GetAllProducts(); // First item that I want to guarantee that happens
         List<ProductModel> SearchProducts(string searchTerm); // Returns a list of anything that matches the saerch term.
         ProductModel GetProductById(int id); // Get product by ID
-        int Insert(ProductModel model); // INSERT
-        int Delete(ProductModel model); // DELETE
-        int Update(ProductModel model); // UPDATE
+        int Insert(ProductModel product); // INSERT
+        int Delete(ProductModel product); // DELETE
+        int Update(ProductModel product); // UPDATE
     }
 }
